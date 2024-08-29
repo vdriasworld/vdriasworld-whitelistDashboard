5d6d75e5ab65f9a15a888ad6c97c2318cddfc2b1
lemon-miaow <lemonmiaow@gmail.com>
2024-08-29 12:51:31 +0800

    remove Profile.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import List from "../views/List.vue";
-import Profile from "../views/Profile.vue";
 import Signin from "../views/Signin.vue";
 
 const routes = [
