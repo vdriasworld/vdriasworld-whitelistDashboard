410bab88a7cae8b12d72e3ec334ff622547e981c
lemon-miaow <lemonmiaow@gmail.com>
2024-08-29 12:45:59 +0800

    rename WhitelistRequest.vue

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AuthorsTable from "./components/AuthorsTable.vue";
+import AuthorsTable from "./components/WhitelistRequest.vue";
 </script>
 <template>
   <div class="py-4 container-fluid">
