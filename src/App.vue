bcef35ab89a53944836dd847753a65858a8b5de3
lemon-miaow <lemonmiaow@gmail.com>
2024-09-01 03:39:39 +0800

    yee

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
-<!--
-=========================================================
-* Vue Argon Dashboard 2 - v4.0.0
-=========================================================
-
-* Product Page: https://creative-tim.com/product/vue-argon-dashboard
-* Copyright 2024 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <script setup>
 import { computed } from "vue";
 import { useStore } from "vuex";
